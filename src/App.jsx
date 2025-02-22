faecccd8cbd3b1e7310ce60de5a259eec779f538
Dasharath Suthar <suthardasharath4@gmail.com>
2025-02-22 12:35:06 +0530

    adding new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import Todo from './components/Todo'
+
 import './App.css'
+import FullForm from './components/FullForm'
+import Joke from './components/Joke'
 
 function App() {
 
 
   return (
     <>
-      <Todo></Todo>
+      <Joke></Joke>
     </>
   )
 }
